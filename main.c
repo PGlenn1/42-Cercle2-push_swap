df780ffeacd011351365890f40d8b19379792aa0
Glenn PIRIOU <glenn.glpiriou@student.42.fr>
2023-02-20 14:52:52 +0100

    initizalization works

--- main.c
+++ main.c
@@ -8,13 +8,10 @@ t_stack **init_stack_a(char **split_tab)
     t_stack *new;
     t_stack **head;
 
-    printf("INIT STACK A\n");
     if (!split_tab)
         return (NULL);
     first = ps_lstnew(split_tab[0]);
     head = &first;
-    printf("head:%p\n", head);
-    printf("*head:%p\n", *head);
     new = NULL;
     int i = 1;
     while (split_tab[i])
@@ -22,11 +19,8 @@ t_stack **init_stack_a(char **split_tab)
         new = ps_lstnew(split_tab[i]);
         // printf("new_str:%s\n", new->content);
         ps_lstadd_back(head, new);
-        printf("WHILE *head:%p\n", *head);
         i++;
     }
-    printf("*head:%p\n", *head);
-    printf("END INIT STACK A\n\n");
     return (head);
 }
 
@@ -34,53 +28,39 @@ void print_stack(t_stack **stack)
 {
     t_stack *current;
 
-    printf("\nPRINT STACK\n");
+	printf("\nPRINT STACK\n");
     current = *stack;
-    printf("stack:%p\n", stack);
     while (current)
     {
-        printf("*stack:%p\n", *stack);
-        printf("current:%p\n", current);
-        // printf("str:%s\n", current->content);
-        printf("current->next:%p\n", current->next);
+        printf("str:%s\n", current->content);
         current = current->next;
     }
-    printf("END PRINT STACK\n\n");
+	printf("\nEND PRINT STACK\n");
 }
 
-// void push_swap(t_stack **stack_a, t_stack **stack_b)
-// {
+void push_swap(char **split_input)
+{
+    t_stack **stack_a;
+	t_stack **stack_b;
+	t_stack *b_elem0;
 
-// }
+    stack_a = init_stack_a(split_input);
+    print_stack(stack_a);
+	b_elem0 = ps_lstnew(NULL);
+	stack_b = &b_elem0;
+	print_stack(stack_b);
+}
 
 int main(int argc, char **argv)
 {
     char **split_input;
-    t_stack **stack_a;
-    t_stack **stack_b;
-    t_stack *b0_init;
 
     (void)argc;
-    printf("MAIN\n");
     split_input = ft_split(argv[1], ' ');
-    printf("str:%s\n", split_input[0]);
     if (!split_input)
+	{
         return (-1);
-    stack_a = init_stack_a(split_input);
-    printf("\nMAIN PRINT STACK VALUES:\n");
-    printf("stack_a:%p\n", stack_a);
-    printf("*stack_a:%p\n", *stack_a);
-    printf("*stack_a->content:%p\n", (*stack_a)->content);
-    // printf("*stack_a->content:%s\n", (*stack_a)->content);
-    // printf("*stack_a->valu:%d\n", (*stack_a)->value);
-    b0_init = ps_lstnew(NULL);
-    stack_b = &b0_init;
-    // push_swap(stack_a, stack_b);
-    // printf("head:%p\n", *stack_a);
-    print_stack(stack_a);
-    printf("\nMAIN PRINT STACK VALUES:\n");
-    printf("stack_a:%p\n", stack_a);
-    printf("*stack_a:%p\n", *stack_a);
-    printf("*stack_a->content:%p\n", (*stack_a)->content);
+	}
+	push_swap(split_input);
     return (0);
 }
