9c1efc838450899429b533207656cc30fa58e7d8
Glenn PIRIOU <glenn.glpiriou@student.42.fr>
2023-02-21 17:56:34 +0100

    fixed operators.c

--- operators.c
+++ operators.c
@@ -15,16 +15,16 @@ void	swap_a(t_root *root)
 	prev->index = tmp_index;
 }
 
-void	swap_a(t_root *root)
+void	swap_b(t_root *root)
 {
 	t_stack	*last;
 	t_stack	*prev;
 	int		tmp_index;
 
-	last = ps_lstlast(root->stack_a);
+	last = ps_lstlast(root->stack_b);
 	if (last->index < 1)
 		return ;
-	prev = ps_lstprev(root->stack_a);
+	prev = ps_lstprev(root->stack_b);
 	tmp_index = last->index;
 	last->index = prev->index;
 	prev->index = tmp_index;
