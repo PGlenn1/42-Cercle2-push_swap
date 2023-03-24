969cdd51d2489e6870758da7ad36ca3ee3b9d20a
gpiriou <piriou.glenn@gmail.com>
2023-03-24 06:37:31 +0100

    sort_three wip

--- main.c
+++ main.c
@@ -10,25 +10,46 @@ int	is_sorted(t_stack *stack)
 	while (probe && probe->next)
 	{
 		value = probe->value;
-		if (value > probe->next->value)
+		if (value < probe->next->value)
 			return (0);
 		probe = probe->next;
 	}
 	return (1);
 }
 
-void	sort_two(t_root *root)
+void sort_three(t_root *root, t_stack *stack)
 {
-	printf("SORT TWO\n");
-	if (!is_sorted(root->stack_a))
-	{
-		swap_ab(root, A);
-	}
+	int first;
+	int sec_last;
+	int last;
 
+	printf("SORT THREE\n");
+	first = stack->first->value;
+	sec_last = stack->sec_last->value;
+	last = stack->last->value;
+	
+	printf("first:%d\n", first);
+	printf("sec_last:%d\n", sec_last);
+	printf("last:%d\n", last);
+
+	if ( first < sec_last && first < last && sec_last < last)
+	{
+		rotate_ab(root, A);
+		rotate_ab(root, A);
+	}
+	else if (sec_last < last && sec_last < first && first < last)
+	{
+		rotate_ab(root, A);
+	}
+	else if (sec_last < last < first)
+		rev_rotate_ab(root, A);
+	else
+		ft_error(UNWANTED_BEHAVIOR);
 }
 
-void pick_algo(t_root *root)
+int pick_algo(t_root *root)  /// LAST MUST BE SMALLEST
 {
+	print_both(root);
 	// if (root->stack_a->size > 500)
 	// 	sort_bigger(root);
 	// if (root->stack_a->size == 500)
@@ -43,13 +64,15 @@ void pick_algo(t_root *root)
 	// 	sort_medium(root);
 	// else if (root->stack_a->size > 3)
 	// 	sort_small(root);
-	// if (root->stack_a->size == 3)
-	// 	sort_simple(root);
-	if (root->stack_a->size == 2)
-		sort_two(root);
+	if (root->stack_a->size == 3)
+		sort_three(root, root->stack_a);
+	print_both(root);
+	if (is_sorted(root->stack_a))
+		return (1);
+	return (0);
 }
 
-void	push_swap(char **input)
+int	push_swap(char **input)
 {
 	t_root *root;
 
@@ -58,8 +81,9 @@ void	push_swap(char **input)
 		ft_error(MALLOC_FAIL);
 	if (is_sorted(root->stack_a))
 		ft_error(ALREADY_SORTED);
-	else
-		pick_algo(root);
+	else if (pick_algo(root))
+			return (1);
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -67,6 +91,9 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		return (0);
-	push_swap(argv);
+	if (push_swap(argv))
+		printf("Ok\n");
+	else
+		printf("Unsorted\n");
 	return (0);
 }
