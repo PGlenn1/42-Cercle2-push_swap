c4f844c361224e5d45c136805a8b2a65c69a452a
Glenn PIRIOU <glenn.glpiriou@student.42.fr>
2023-05-31 19:53:22 +0200

    libft management(del)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: glpiriou <glpiriou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 16:18:15 by glpiriou          #+#    #+#             */
-/*   Updated: 2023/05/31 16:18:16 by glpiriou         ###   ########.fr       */
+/*   Updated: 2023/05/31 19:44:36 by glpiriou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,10 @@ int	push_swap(char **input)
 int	main(int argc, char **argv)
 {
 	if (argc < 3)
+	{
+		write(2, "Error\n", 6);
 		return (0);
+	}
 	if (push_swap(argv))
 		return (0);
 	return (1);
